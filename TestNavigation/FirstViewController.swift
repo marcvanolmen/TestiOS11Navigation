1a9c0edebd35dc93083ddf85dec089123a240096
Marc Van Olmen <marc.vanolmen@thomsonreuters.com>
2017-10-09 15:24:53 -0400

    [add] initial test to replace title item by custom view.

--- TestNavigation/FirstViewController.swift
+++ TestNavigation/FirstViewController.swift
@@ -13,6 +13,16 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        let aView = UIView(frame: CGRect.zero)
+        
+        NSLayoutConstraint(item: aView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 60).isActive = true
+        NSLayoutConstraint(item: aView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 20).isActive = true
+        
+        self.navigationItem.titleView = aView
+        aView.backgroundColor = .red
     }
 
     override func didReceiveMemoryWarning() {
